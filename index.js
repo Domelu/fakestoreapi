c00b625f2696d85f160e765f6da58a9c3f802732
Domenika <Domenika192002p@gmail.com>
2024-03-06 22:11:16 -0500

    mara

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-console.log("holi");
+
 
 const URL= "https://fakestoreapi.com/products";
+let titulo = document.getElementById("titulo") 
+let precio = document.getElementById("precio") 
+console.log() 
+
 
 function getDATA (){
-
     fetch(URL)
     .then( response => response.json() )
-    .then(data => console.log("DATOS DE LAS API:",data) )
+    .then(data => {
+      console.log("datos de la api= ",data) 
+      for(let i = 0; i < data.length; i++){
+    console.log(data[i].title)
+  
+      }
+      titulo.innerHTML= data[1].title;
+      titulo.innerHTML= data[2].price; 
+})
+//titulo.innerHTML= "nuevo texto";
+
+
 }
+
 getDATA();
